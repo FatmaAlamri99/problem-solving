69cef98783b9704d5fa07dcfd18048ae59d189d4
FatmaAlamri99 <141591644+FatmaAlamri99@users.noreply.github.com>
2023-08-12 18:21:18 +0400

    Add files via upload

---
+++ fatma yousuf-problem solving/problem solving day5/script.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+function fraction(){
+
+
+
+
+
+const numerator=[1,2,2,8]
+const denominator=[2,4,6,12]
+
+
+
+    const n1=numerator[0]/denominator[0]+numerator[1]/denominator[1]
+    const n2=numerator[2]/denominator[2]+numerator[3]/denominator[3]
+    console.log(n1)
+    console.log(n2)
+
+}
+
+
+fraction()
+
+
+
+
+
