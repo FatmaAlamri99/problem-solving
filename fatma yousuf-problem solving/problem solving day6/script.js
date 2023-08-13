f85496eb8521f470ee236eff21035228cc53b914
FatmaAlamri99 <141591644+FatmaAlamri99@users.noreply.github.com>
2023-08-14 01:44:19 +0400

    Add files via upload

---
+++ fatma yousuf-problem solving/problem solving day6/script.js
@@ -0,0 +1,30 @@
+
+
+
+function checkSum(x,y){
+
+
+    const dice=[1,2,3,4,5,6]
+    const sum =x+y
+   
+    if(x+y <= 6){
+
+
+        console.log("it is = "+sum+" ,the turn is not good")
+
+
+    }
+    else{
+
+        console.log("it is = "+sum+" ,the turn is good")
+
+
+
+    }
+    }
+    
+    
+    checkSum(1,2)
+    checkSum(1,5)
+    checkSum(5,3)
+    
